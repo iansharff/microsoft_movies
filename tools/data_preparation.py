ab349c6ff5b8d19024a349f9c10c043417f95801
iansharff <ips11@georgetown.edu>
2021-06-19 22:35:58 -0400

    Add global constants for CSV file paths
    For convenience, I defined constants in the data_preparation.py
    file that point to strings of the file paths, so from within the
    tools directory the files should be easily accessible from outside
    the data/ directory. This can be changed in the future depending
    on the layout of the repository.

--- tools/data_preparation.py
+++ tools/data_preparation.py
@@ -14,6 +14,18 @@ import numpy as np
 #         [1, 1, 1, 1, n]]
 # test = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'], data=data)
 
+# Define global constants for relative paths from within the tools package
+RT_REVIEWS_PATH = "../data/rt.reviews.tsv"
+RT_MOVIE_INFO = "../data/rt.movie_info.tsv"
+BOM_GROSS = "../data/bom.movie_gross.csv"
+IMDB_NAME_BASICS = "../data/imdb.name.basics.csv"
+IMDB_TITLE_AKAS = "../data/imdb.title.akas.csv"
+IMDB_TITLE_BASICS = "../data/imdb.title.basics.csv"
+IMDB_TITLE_CREW = "../data/imdb.title.crew.csv"
+IMDB_TITLE_PRINCIPALS = "../data/imdb.title.principals.csv"
+IMDB_TITLE_RATINGS = "../data/imdb.title.ratings.csv"
+TMDB_MOVIES = "../data/tmdb.movies.csv"
+TN_BUDGETS = "../data/tn.movie_budgets.csv"
 
 def percent_nan(df):
     """Return a Series of percent NaN values in each column of a DataFrame"""
